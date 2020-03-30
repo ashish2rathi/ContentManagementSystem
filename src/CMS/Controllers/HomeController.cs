a39a2335bd3c7aa311468cd244f417f84d504bd9
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2020-03-30 13:55:35 +0530

    Removed redundant view models and view from code

--- src/CMS/Controllers/HomeController.cs
+++ src/CMS/Controllers/HomeController.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using CMS.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -28,10 +14,9 @@ namespace CMS.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Contact()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
